eaaaca50b3b5df3572137201a96a89c06540233e
Ayush Agarwal <ayush.ag1301@gmail.com>
2021-01-24 22:25:14 +0530

    Sauravs upload

---
+++ scraper2.py
@@ -0,0 +1,83 @@
+from newspaper import Article
+import nltk
+
+nltk.download('punkt')
+
+url = 'https://www.scmp.com/coronavirus'
+article = Article(url, language="en") # en for English 
+'''
+titleList = []
+testList = []
+summaryList = []
+keyList = []
+'''
+article.download()
+article.parse()
+article.nlp() 
+
+print('\n'+ article.title + '\n') #prints the title of the article
+
+'''
+print("Article Text:") 
+print(article.text) #prints the entire text of the article
+print("\n") 
+print("Article Summary:") 
+print(article.summary) #prints the summary of the article
+print("\n") 
+print("Article Keywords:")
+print(article.keywords) #prints the keywords of the article
+'''
+
+points = article.summary.split('.')
+
+
+#for point in points:
+#    with open(r'C:\Users\saura\Documents\ML2021\NewsScraper\output.txt', 'w') as file_object:
+#        file_object.write(point)
+
+
+
+#### SENTIMENT ANALYSIS ####
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import pandas as pd
+from tensorflow import keras
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM,Dense,Dropout
+from tensorflow.keras.layers import SpatialDropout1D
+from tensorflow.keras.layers import Embedding
+
+import speech_recognition as sr
+# Loading model
+reconstructed_model = keras.models.load_model("C:/Users/saura/Documents/ML2021")
+
+text = "UK MP devastated that coronavirus isn't infecting more people"
+
+
+df= pd.read_csv('Tweets.csv', sep=',')
+
+#select relavant columns
+tweet_df = df[['text','airline_sentiment']]
+tweet_df = tweet_df[tweet_df['airline_sentiment'] != 'neutral'] #filtering out neutral comments
+
+# convert airline_seentiment to numeric
+sentiment_label = tweet_df.airline_sentiment.factorize()  # positive:0,negative:1
+
+# Setting up word embeddings
+tweet = tweet_df.text.values
+tokenizer = Tokenizer(num_words=5000)
+tokenizer.fit_on_texts(tweet)
+vocab_size = len(tokenizer.word_index) + 1
+encoded_docs = tokenizer.texts_to_sequences(tweet)
+padded_sequence = pad_sequences(encoded_docs, maxlen=200)
+# Model Training
+for text in points:
+    test_word = text
+    tw = tokenizer.texts_to_sequences([test_word])
+    tw = pad_sequences(tw,maxlen=200)
+    prediction = int(reconstructed_model.predict(tw).round().item())
+    print(text, "- Prediction:", sentiment_label[1][prediction])
